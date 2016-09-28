5954cbe01e4678cc1515f0248e306be0b210e64b
basileiosapostolakis <bapostolakis@gmail.com>
2016-09-28 15:57:52 +0300

    den exoun ginei......

---
+++ bootcamp/6.py
@@ -0,0 +1,9 @@
+i=input("Enter number of triangular numbers :")
+Ti=(int(i)*(int(i)+1))/2
+T="Sum(1+2+3+4+5.......+i)"
+k=1
+print(Ti)
+
+
+
+    
---
+++ bootcamp/7.py
@@ -0,0 +1,7 @@
+i=input("Enter number of triangular numbers :")
+Ti=(int(i)*(int(i)+1))
+Tk=(int(k)*(int(k)+1))
+
+
+print(Ti)
+
