8d01d4245612fc075793f41912db59649ef46a01
basileiosapostolakis <bapostolakis@gmail.com>
2016-09-28 16:31:00 +0300

    Delete 2.py

--- bootcamp/2.py
+++
@@ -1,9 +0,0 @@
-a=input("Enter number of 50 euro banknotes:")
-b=input("Enter number of 20 euro banknotes:")
-c=input("Enter number of 10 euro banknotes:")
-d=input("Enter number of 5 euro banknotes:")
-e=input("Enter number of 2 euro banknotes:")
-f=input("Enter number of 1 euro banknotes:")
-Total=(int(a)*50)+(int(b)*20)+(int(c)*10)+(int(d)*5)+(int(e)*2)+(int(f)*1)
-print("You have", Total)
-
